5032986e92598fceeeb95538a9b0ffbac9057d2d
Narik <queroldo@etud.insa-toulouse.fr>
2013-02-17 20:52:46 +0100

    Committing Hello world eclipse project

---
+++ Hello World/Debug/Hello_World.d
@@ -0,0 +1,19 @@
+Hello_World.d: ../Hello_World.c \
+ /home/narik/Augmented\ Reality\ tools/ARToolKit/include/AR/config.h \
+ /home/narik/Augmented\ Reality\ tools/ARToolKit/include/AR/video.h \
+ /home/narik/Augmented\ Reality\ tools/ARToolKit/include/AR/ar.h \
+ /home/narik/Augmented\ Reality\ tools/ARToolKit/include/AR/param.h \
+ /home/narik/Augmented\ Reality\ tools/ARToolKit/include/AR/sys/videoGStreamer.h \
+ /home/narik/Augmented\ Reality\ tools/ARToolKit/include/AR/gsub_lite.h
+
+/home/narik/Augmented\ Reality\ tools/ARToolKit/include/AR/config.h:
+
+/home/narik/Augmented\ Reality\ tools/ARToolKit/include/AR/video.h:
+
+/home/narik/Augmented\ Reality\ tools/ARToolKit/include/AR/ar.h:
+
+/home/narik/Augmented\ Reality\ tools/ARToolKit/include/AR/param.h:
+
+/home/narik/Augmented\ Reality\ tools/ARToolKit/include/AR/sys/videoGStreamer.h:
+
+/home/narik/Augmented\ Reality\ tools/ARToolKit/include/AR/gsub_lite.h:
